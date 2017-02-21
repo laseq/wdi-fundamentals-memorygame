aed221c6fa01c4bc1fcb18b5ac8e6ff9712fcff1
Lourenco Sequeira <laseq@lourencos-mbp.home>
2017-02-21 15:43:25 +0000

    Added functionality to create the card divs in Javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,31 @@
 console.log("JS file is connected to HTML!")
 
+//Variables for the game's logic
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardThree){
+/*if (cardOne === cardThree){
 	alert('You found a match.');
 } else {
 	alert("Sorry try again");
+}*/
+
+var createCards = function(){
+	console.log("Entered function createCards")
+	//Attach the game-board div from the HTML file to a variable here
+	var gameBoard = document.getElementById('game-board');
+	var newCardDiv = null;
+
+	//This for loop creates the 4 card divs and places them in the game-board div
+	for (var i=0; i<4; i++){
+		console.log("Entered the for loop. i = " + i);
+		newCardDiv = document.createElement('div');
+		newCardDiv.className = 'card';
+		gameBoard.appendChild(newCardDiv);
+		console.log("Reached the end of the for loop");
+	}
 }
+
+createCards();
